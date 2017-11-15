646b1d0b1d471481d81a978125ecececf401db5d
Unknown <miejscov@gmail.com>
2017-11-15 14:51:13 +0100

    Linux working

--- Assets/Flight.cs
+++ Assets/Flight.cs
@@ -1,24 +1,21 @@
 ﻿using UnityEngine;
 
-public class Flight : MonoBehaviour {
-
-    public float speed = 5f;
-    public float power = 7f;
+public class Flight : MonoBehaviour
+{
+    public float rotationSpeed = 15f;
+    public float rocketPower = 10.5f;
 
     private Rigidbody2D rigid;
 
-	void Start () {
+    void Start()
+    {
         rigid = GetComponent<Rigidbody2D>();
-	}
-	
-	void Update () {
-        rigid.AddRelativeForce(Vector2.up * power);
+    }
 
-        transform.rotation = Quaternion.Euler(0, 0, Input.GetAxis("Horizontal") * (-speed));
+    void Update()
+    {
+        rigid.AddRelativeForce(Vector2.up * rocketPower);
 
-        /*if (Input.GetKey("left"))
-            transform.Translate(Vector2.left * speed);
-        if (Input.GetKey("right"))
-            transform.Translate(Vector2.right * speed);
-    */}
+        transform.rotation = Quaternion.Euler(0, 0, Input.GetAxis("Horizontal") * (-rotationSpeed));
+    }
 }
